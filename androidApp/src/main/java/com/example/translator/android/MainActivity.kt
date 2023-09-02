9a9cae9b9836a821341ae013f08f7ef18c4101de
burak <karadumanburak000@gmail.com>
2023-09-02 20:11:22 +0300

    - Designs added for selecting languages

--- androidApp/src/main/java/com/example/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/translator/android/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
