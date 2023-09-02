04870038056a3046b9dcf999d9ce780f70aa840b
burak <karadumanburak000@gmail.com>
2023-09-02 20:28:58 +0300

    - NavHost added into activity

--- androidApp/src/main/java/com/example/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/translator/android/MainActivity.kt
@@ -5,7 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.translator.android.core.presentation.Routes
+import com.example.translator.android.translate.presentation.AndroidTranslateViewModel
+import com.example.translator.android.translate.presentation.TranslateScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,8 +28,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    TranslateRoot()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TranslateRoot() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Routes.TRANSLATE
+    ) {
+        composable(route = Routes.TRANSLATE) {
+            val viewModel = hiltViewModel<AndroidTranslateViewModel>()
+            val state by viewModel.uiState.collectAsState()
+
+            TranslateScreen(
+                state = state,
+                onEvent = viewModel::onEvent
+            )
         }
     }
 }
