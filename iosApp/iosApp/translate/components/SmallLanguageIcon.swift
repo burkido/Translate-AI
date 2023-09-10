50fa90661de187d12d67700e53892bf1e8afd0b6
burak <karadumanburak000@gmail.com>
2023-09-10 19:10:43 +0300

    - Some ui added for translate feature in ios

---
+++ iosApp/iosApp/translate/components/SmallLanguageIcon.swift
@@ -0,0 +1,19 @@
+//
+//  SmallLanguageIcon.swift
+//  iosApp
+//
+//  Created by Burak Karaduman on 10.09.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct SmallLanguageIcon: View {
+    var language: UiLanguage
+    var body: some View {
+        Image(uiImage: UIImage(named: language.imageName.lowercased())!)
+            .resizable()
+            .frame(width: 30, height: 30)
+    }
+}
