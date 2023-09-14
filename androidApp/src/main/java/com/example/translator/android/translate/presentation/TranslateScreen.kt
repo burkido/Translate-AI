499ff0a8482c66eeeaa54f1de7ded5cf91171074
burak <karadumanburak000@gmail.com>
2023-09-14 22:29:48 +0300

    - Translate screen updated

--- androidApp/src/main/java/com/example/translator/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/example/translator/android/translate/presentation/TranslateScreen.kt
@@ -1,29 +1,90 @@
 package com.example.translator.android.translate.presentation
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.translator.android.R
 import com.example.translator.android.translate.presentation.components.LanguageDropDown
+import com.example.translator.android.translate.presentation.components.SmallLanguageIcon
 import com.example.translator.android.translate.presentation.components.SwapLanguagesButton
 import com.example.translator.android.translate.presentation.components.TranslateTextField
+import com.example.translator.android.translate.presentation.components.gradientSurface
+import com.example.translator.translate.domain.translate.TranslateError
 import com.example.translator.translate.presentation.TranslateEvent
 import com.example.translator.translate.presentation.TranslateState
+import com.example.translator.translate.presentation.UiHistoryItem
+
+@Composable
+fun TranslateHistoryItem(
+    item: UiHistoryItem,
+    onClick: () -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+            .shadow(elevation = 4.dp, shape = RoundedCornerShape(24.dp))
+            .clip(RoundedCornerShape(24.dp))
+            .gradientSurface()
+            .clickable(onClick = onClick)
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            SmallLanguageIcon(language = item.fromLanguage)
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                text = item.fromText,
+                style = MaterialTheme.typography.body2
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                SmallLanguageIcon(language = item.toLanguage)
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = item.toText,
+                    color = MaterialTheme.colors.onSurface,
+                    style = MaterialTheme.typography.body1,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -33,9 +94,33 @@ fun TranslateScreen(
 ) {
     val context = LocalContext.current
 
+    LaunchedEffect(key1 = state.error) {
+        val message = when (state.error) {
+            TranslateError.SERVICE_UNAVAILABLE -> context.getString(R.string.error_service_unavailable)
+            TranslateError.CLIENT_ERROR -> context.getString(R.string.client_error)
+            TranslateError.SERVER_ERROR -> context.getString(R.string.server_error)
+            TranslateError.UNKNOWN -> context.getString(R.string.unknown_error)
+            else -> null
+        }
+        message?.let {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            onEvent(TranslateEvent.OnErrorSeen)
+        }
+    }
+
     Scaffold(
         floatingActionButton = {
-
+            FloatingActionButton(
+                onClick = { onEvent(TranslateEvent.RecordAudio) },
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.size(56.dp)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.mic),
+                    contentDescription = "Record Audio"
+                )
+            }
         }
     ) { paddingValues ->
         LazyColumn(
@@ -98,6 +183,25 @@ fun TranslateScreen(
                     onTextFieldClick = { onEvent(TranslateEvent.EditTranslation) },
                 )
             }
+
+            item {
+                if (state.history.isNotEmpty()) {
+                    Text(text = "History", style = MaterialTheme.typography.h2)
+                }
+            }
+
+            items(
+                count = state.history.size,
+                key = { index -> state.history[index].id },
+                itemContent = { index ->
+                    val item = state.history[index]
+                    TranslateHistoryItem(
+                        item = item,
+                        onClick = { onEvent(TranslateEvent.SelectHistoryItem(item)) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            )
         }
     }
 }
