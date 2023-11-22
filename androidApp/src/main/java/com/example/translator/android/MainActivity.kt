7797106c7b95a7a9545fa10544bcab8df0a4a298
burak <karadumanburak000@gmail.com>
2023-11-22 23:47:19 +0300

    - Navigation utils added for bottom nav bar

--- androidApp/src/main/java/com/example/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/translator/android/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.translator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,13 +15,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.translator.android.core.components.BottomNavigationBar
+import com.example.translator.android.core.components.NavigationActions
+import com.example.translator.android.core.components.TopLevelDestinations
 import com.example.translator.android.core.presentation.Routes
 import com.example.translator.android.translate.presentation.AndroidTranslateViewModel
 import com.example.translator.android.translate.presentation.TranslateScreen
@@ -36,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TranslateRoot()
+                    MainApp()
                 }
             }
         }
@@ -44,12 +56,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TranslateRoot() {
+fun MainApp() {
     val navController = rememberNavController()
+    val navigationActions = remember(navController) { NavigationActions(navController) }
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val selectedDestination =
+        navBackStackEntry?.destination?.route ?: TopLevelDestinations.TRANSLATE.route
+
+    AppContent(
+        navController = navController,
+        selectedDestination = selectedDestination,
+        navigateToTopLevelDestination = navigationActions::navigateTo
+    )
+}
+
+@Composable
+fun AppContent(
+    navController: NavHostController,
+    selectedDestination: String,
+    navigateToTopLevelDestination: (TopLevelDestinations) -> Unit,
+) {
+    val showBottomBar = remember {
+        mutableStateOf(true)
+    }
+
+    val bottomBarRoutes = listOf(
+        TopLevelDestinations.TRANSLATE.route,
+        TopLevelDestinations.SAVED.route,
+    )
+
+    navController.addOnDestinationChangedListener { _, destination, _ ->
+        showBottomBar.value = destination.route in bottomBarRoutes
+    }
+
+    Row(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.inverseOnSurface),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            TranslateRoot(
+                navController = navController,
+                modifier = Modifier.weight(1f),
+            )
+            AnimatedVisibility(visible = showBottomBar.value) {
+                BottomNavigationBar(
+                    destinations = TopLevelDestinations.values().toList(),
+                    selectedDestination = selectedDestination,
+                    onNavigateToTopLevelDestination = navigateToTopLevelDestination
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TranslateRoot(
+    modifier: Modifier,
+    navController: NavHostController,
+) {
     NavHost(
         navController = navController,
-        startDestination = Routes.TRANSLATE
+        startDestination = Routes.TRANSLATE,
+        modifier = modifier
     ) {
+        // TODO: add nav graph builder here
         composable(route = Routes.TRANSLATE) {
             val viewModel = hiltViewModel<AndroidTranslateViewModel>()
             val state by viewModel.uiState.collectAsState()
