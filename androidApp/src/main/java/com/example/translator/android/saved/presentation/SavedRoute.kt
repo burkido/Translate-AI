74d0c2ada50318d8679a7cb293b186bb4868c765
burak <karadumanburak000@gmail.com>
2023-11-24 22:06:43 +0300

    - Saved route added

---
+++ androidApp/src/main/java/com/example/translator/android/saved/presentation/SavedRoute.kt
@@ -0,0 +1,29 @@
+package com.example.translator.android.saved.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SavedRoute() {
+
+    // TODO: view model will be added here
+    SavedScreen()
+}
+
+@Composable
+fun SavedScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Saved screen")
+    }
+
+}
