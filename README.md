8c28c6b5bc209dd34e4d0b1d23ed6e3174078760
burak <karadumanburak000@gmail.com>
2024-01-05 22:45:07 +0300

    - Are you sure dialog added when deleting saved notes

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-README.md
+# Translate AI
+
+**Description:**
+
+Translate AI is a Kotlin Multiplatform Mobile (KMM) project that embraces Clean Architecture principles and follows the Model-View-Intent (MVI) pattern. This project structure is designed to provide a scalable and maintainable foundation for Android development and IOS (IOS part is missing. Must be continued). Also, Material 3 theming is applied to the Android side
+
+General structure of project is described below. It is a sample from shared module. You can see the whole project for better understanding.
+
+## Packages
+
+### 1. Core
+
+- **Domain:** Contains the core business logic and domain entities.
+- **Presentation:** Handles UI-related logic for the core module.
+
+### 2. Translate (Feature)
+
+- **Data:**
+  - **Remote:** Manages data retrieval from remote sources (e.g., API).
+  - **Local:** Handles local data storage and retrieval.
+- **Domain:**
+  - **History:** Contains data sources and item-related logic for translation history.
+  - **Translate:** Includes use cases and possible errors related to translation.
+- **Presentation:** Manages the UI logic for the translation feature.
+
+### 3. VoiceToText (Feature)
+
+- **Domain:** Handles business logic specific to the voice-to-text feature.
+- **Presentation:** Manages the UI logic for the voice-to-text feature.
+
+### 4. Util
+
+- Includes utility classes and functions that can be shared across different parts of the project.
--- androidApp/src/main/java/com/example/translator/android/saved/presentation/SavedRoute.kt
+++ androidApp/src/main/java/com/example/translator/android/saved/presentation/SavedRoute.kt
@@ -1,6 +1,5 @@
 package com.example.translator.android.saved.presentation
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,12 +10,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -32,9 +35,10 @@ fun SavedRoute(
     state: SavedState,
     onEvent: (SavedEvent) -> Unit
 ) {
+
+
     Scaffold(
-        topBar = { SavedTopBar(onEvent = onEvent)
-        },
+        topBar = { SavedTopBar(onEvent = onEvent) },
     ) { paddingValues ->
         SavedScreen(
             innerPadding = paddingValues,
@@ -76,6 +80,29 @@ fun SavedScreen(
 fun SavedTopBar(
     onEvent: (SavedEvent) -> Unit
 ) {
+    val openDialog = remember { mutableStateOf(false) }
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = { openDialog.value = false },
+            title = { Text(text = stringResource(id = R.string.delete_all)) },
+            text = { Text(text = stringResource(id = R.string.are_you_sure)) },
+            confirmButton = {
+                TextButton(onClick = {
+                    onEvent(SavedEvent.DeleteAllTranslations)
+                    openDialog.value = false
+                }) {
+                    Text(text = stringResource(id = R.string.confirm))
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { openDialog.value = false }) {
+                    Text(text = stringResource(id = R.string.cancel))
+                }
+            }
+        )
+    }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -88,7 +115,7 @@ fun SavedTopBar(
             style = MaterialTheme.typography.titleMedium
         )
         Spacer(modifier = Modifier.weight(1f))
-        IconButton(onClick = { onEvent(SavedEvent.DeleteAllTranslations) }) {
+        IconButton(onClick = { openDialog.value = true }) {
             Icon(
                 imageVector = Icons.Filled.Delete,
                 contentDescription = stringResource(id = R.string.delete_all),
